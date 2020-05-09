e8fbdeafa706bc878fbba81ef3615d28eed49e68
Ashish D'Souza <sudouser512@gmail.com>
2020-05-09 16:33:27 -0400

    Update

---
+++ src/main/java/com/ashishdsouza/TimeLock/EncryptionInformation.java
@@ -0,0 +1,41 @@
+package com.ashishdsouza.TimeLock;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "encryption")
+public class EncryptionInformation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private String privateKey;
+    private Integer timestamp;
+    private String checksum;
+
+    public String getPrivateKey() {
+        return privateKey;
+    }
+
+    public void setPrivateKey(String privateKey) {
+        this.privateKey = privateKey;
+    }
+
+    public Integer getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Integer timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getChecksum() {
+        return checksum;
+    }
+
+    public void setChecksum(String checksum) {
+        this.checksum = checksum;
+    }
+}
