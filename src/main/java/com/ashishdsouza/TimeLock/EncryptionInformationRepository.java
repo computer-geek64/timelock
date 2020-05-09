8430ddb61a45147503e5c5b00980d0100f93e263
Ashish D'Souza <sudouser512@gmail.com>
2020-05-09 16:49:30 -0400

    Update

--- src/main/java/com/ashishdsouza/TimeLock/EncryptionInformationRepository.java
+++ src/main/java/com/ashishdsouza/TimeLock/EncryptionInformationRepository.java
@@ -1,7 +1,6 @@
 package com.ashishdsouza.TimeLock;
 
 import org.springframework.data.repository.CrudRepository;
-import com.ashishdsouza.TimeLock.EncryptionInformation;
 
 public interface EncryptionInformationRepository extends CrudRepository<EncryptionInformation, String> {
 }
