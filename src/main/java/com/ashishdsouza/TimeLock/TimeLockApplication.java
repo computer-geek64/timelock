0a317b4cea8e08feb1b9fac7db9c1e5ce9dd3125
Ashish D'Souza <sudouser512@gmail.com>
2020-05-08 21:19:21 -0400

    Add Maven

---
+++ src/main/java/com/ashishdsouza/TimeLock/TimeLockApplication.java
@@ -0,0 +1,13 @@
+package com.ashishdsouza.TimeLock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeLockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeLockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ashishdsouza/TimeLock/TimeLockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashishdsouza.TimeLock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeLockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
