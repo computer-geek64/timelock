16f6d6566f8530a6e48a88d12771dc6c4bd1100c
Ashish D'Souza <sudouser512@gmail.com>
2020-05-09 19:48:00 -0400

    Update

--- src/main/java/com/ashishdsouza/TimeLock/Client/RSAEncryption.java
+++ src/main/java/com/ashishdsouza/TimeLock/Client/RSAEncryption.java
@@ -1,7 +1,54 @@
 package com.ashishdsouza.TimeLock.Client;
 
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import java.security.*;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
+
 public class RSAEncryption {
-    public RSAEncryption() {
-        //
+    private static PublicKey base64ToPublicKey(String publicKeyBase64) {
+        try {
+            KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+            byte[] publicKeyBytes = Base64.getDecoder().decode(publicKeyBase64);
+            return keyFactory.generatePublic(new X509EncodedKeySpec(publicKeyBytes));
+        }
+        catch(NoSuchAlgorithmException | InvalidKeySpecException ex) {
+            return null;
+        }
+    }
+
+    private static PrivateKey base64ToPrivateKey(String privateKeyBase64) {
+        try {
+            KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+            byte[] privateKeyBytes = Base64.getDecoder().decode(privateKeyBase64);
+            return keyFactory.generatePrivate(new PKCS8EncodedKeySpec(privateKeyBytes));
+        } catch (NoSuchAlgorithmException | InvalidKeySpecException ex) {
+            return null;
+        }
+    }
+
+    public static byte[] encryptString(String plaintext, String publicKeyBase64) {
+        try {
+            Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+            cipher.init(Cipher.ENCRYPT_MODE, base64ToPublicKey(publicKeyBase64));
+            return cipher.doFinal(plaintext.getBytes());
+        } catch (NoSuchPaddingException | NoSuchAlgorithmException | InvalidKeyException | BadPaddingException | IllegalBlockSizeException ex) {
+            return null;
+        }
+    }
+
+    public static String decryptBytes(byte[] ciphertext, String privateKeyBase64) {
+        try {
+            Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+            cipher.init(Cipher.DECRYPT_MODE, base64ToPrivateKey(privateKeyBase64));
+            return new String(cipher.doFinal(ciphertext));
+        } catch (NoSuchAlgorithmException | IllegalBlockSizeException | BadPaddingException | NoSuchPaddingException | InvalidKeyException ex) {
+            return null;
+        }
     }
 }
