eba3e8fbf348b5368aeb7e3460836f6bf64266b6
Ashish D'Souza <sudouser512@gmail.com>
2020-05-10 11:54:17 -0400

    Update

--- src/main/java/com/ashishdsouza/TimeLock/Client/timelock.java
+++ src/main/java/com/ashishdsouza/TimeLock/Client/timelock.java
@@ -1,18 +1,20 @@
 package com.ashishdsouza.TimeLock.Client;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.net.URI;
 import java.net.URLEncoder;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.security.*;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.PKCS8EncodedKeySpec;
@@ -85,7 +87,9 @@ public class timelock {
 
         try {
             HttpClient httpClient = HttpClient.newHttpClient();
-            HttpRequest httpRequest = HttpRequest.newBuilder().uri(URI.create(url + args)).build();
+            HttpRequest httpRequest = HttpRequest.newBuilder()
+                    .uri(URI.create(url + args))
+                    .build();
 
             HttpResponse<String> httpResponse = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
             return httpResponse.body();
@@ -97,11 +101,19 @@ public class timelock {
 
     public static String postRequest(String url, HashMap<String, String> values) {
         try {
-            ObjectMapper objectMapper = new ObjectMapper();
-            String requestBody = objectMapper.writeValueAsString(values);
+            StringBuilder requestBodyBuilder = new StringBuilder();
+            for(String key : values.keySet()) {
+                requestBodyBuilder.append(URLEncoder.encode(key, StandardCharsets.UTF_8)).append("=").append(URLEncoder.encode(values.get(key), StandardCharsets.UTF_8)).append("&");
+            }
+            String requestBody = requestBodyBuilder.toString();
+            requestBody = requestBody.substring(0, requestBody.length() - 1);
 
             HttpClient httpClient = HttpClient.newHttpClient();
-            HttpRequest httpRequest = HttpRequest.newBuilder().uri(URI.create(url)).POST(HttpRequest.BodyPublishers.ofString(requestBody)).build();
+            HttpRequest httpRequest = HttpRequest.newBuilder()
+                    .uri(URI.create(url))
+                    .header("Content-Type", "application/x-www-form-urlencoded")
+                    .POST(HttpRequest.BodyPublishers.ofString(requestBody))
+                    .build();
 
             HttpResponse<String> httpResponse = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
             return httpResponse.body();
@@ -149,15 +161,29 @@ public class timelock {
 
         if (args[0].equals("encrypt")) {
             // Encrypt file
-            String file = args[1];
+            File file = new File(args[1]);
+            if(!file.exists() || file.isDirectory()) {
+                System.out.println("Invalid file");
+                return;
+            }
+
             double timestamp = (double) System.currentTimeMillis() / 1000 + 120;
 
             HashMap<String, String> postArgs = new HashMap<>();
             postArgs.put("time", String.format("%f", timestamp));
-            System.out.println(postArgs);
 
-            String publicKey = postRequest("http://localhost:8080/generate", postArgs);
-            System.out.println(publicKey);
+            String publicKeyBase64 = postRequest("http://localhost:8080/generate", postArgs);
+
+            try {
+                byte[] fileBytes = Files.readAllBytes(file.toPath());
+                byte[] ciphertext = encryptString(bytesToBase64(fileBytes), publicKeyBase64);
+                OutputStream outputStream = new FileOutputStream(new File(file.toPath() + ".enc"));
+                outputStream.write(ciphertext);
+                outputStream.close();
+            } catch (IOException e) {
+                System.out.println("Error reading file: " + file.toPath());
+                return;
+            }
         } else if (args[0].equals("decrypt")) {
             // Decrypt file
             String file = args[1];
